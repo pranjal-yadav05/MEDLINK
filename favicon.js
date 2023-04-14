fa4c197830e8c92346d9ae31684c8b88b1d1cfe5
Megh ✨ <71490844+sharmamegh@users.noreply.github.com>
2023-04-14 14:08:58 +0530

    Enhanced performance of favicon transition

--- favicon.js
+++ favicon.js
@@ -5,11 +5,11 @@
     };
 
     function iconChange() {
-    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon1.png";}, 333);
-    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon2.png";}, 667);
-    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon3.png";}, 1000);  
-    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon4.png";}, 1333);
-    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon5.png";}, 1667);
-    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon6.png";}, 2000);  
-    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon7.png";}, 2333);
+    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon1.png";}, 142.86);
+    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon2.png";}, 285.71);
+    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon3.png";}, 428.57);  
+    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon4.png";}, 571.43);
+    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon5.png";}, 714.29);
+    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon6.png";}, 857.14);  
+    setTimeout(function(){ document.getElementById("icon").href = "/images/MedLinkAnimatedFavicon7.png";}, 1000);
     }
